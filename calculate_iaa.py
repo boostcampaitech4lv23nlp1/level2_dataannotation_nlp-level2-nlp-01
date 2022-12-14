e5f8b2b1c539d63c1884e4ba5f18771a611a5536
wooy0ng <yygg9800@naver.com>
2022-12-14 12:39:37 +0000

    etc: modify file path

--- calculate_iaa.py
+++ calculate_iaa.py
@@ -2,9 +2,9 @@ import pandas as pd
 import numpy as np
 from fleiss import fleissKappa
 
-result = pd.read_excel('iaa_sample.xlsx',engine='openpyxl')
+result = pd.read_excel('./iaa_data/prev_iaa_data.xlsx',engine='openpyxl')
 result = result.to_numpy()
-num_classes = int(np.max(result))
+num_classes = int(np.max(result))   # number of class
 
 transformed_result = []
 for i in range(len(result)):
